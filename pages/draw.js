a9ff1b22edc0c5365404b18ada9dc9850e9b7be8
AndrewJones-PSU <aej5218@psu.edu>
2022-03-08 00:37:47 -0500

    added some page stuff no idea if this works

--- pages/draw.js
+++ pages/draw.js
@@ -1,10 +1,34 @@
-import styles from '../styles/draw.module.css'
+import Head from 'next/head'
 import Link from 'next/link'
+import Script from 'next/script'
+
+import styles from '../styles/draw.module.css'
 
 export default function draw() {
-    return (
-        <p>
-            There&apos;s nothing here right now, perhaps you&apos;d like to <Link href="/draw"> draw</Link>?
-        </p>
-    )
+	return (
+		<div className={styles.container}>
+
+			<Head>
+				<title>VeriDraw/draw</title>
+				<meta name="description" content="Design Hardware by drawing diagrams" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+			<main className={styles.main}>
+				<div className={styles.topbar}>
+					<div className={styles.button}>
+						<p>File</p>
+					</div>
+					<div className={styles.button}>
+						<p>Edit</p>
+					</div>
+					<div className={styles.button}>
+						<p>View</p>
+					</div>
+				</div>
+			</main>
+
+		</div >
+	)
+
 }
