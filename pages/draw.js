eeb4a45ccfd98a5c37f6b9fda445d02de4f18ee2
AndrewJones-PSU <aej5218@psu.edu>
2022-03-07 23:31:43 -0500

    default

--- pages/draw.js
+++ pages/draw.js
@@ -1,7 +1,7 @@
 import styles from '../styles/draw.module.css'
 import Link from 'next/link'
 
-export function draw() {
+export default function draw() {
     return (
         <p>
             There&apos;s nothing here right now, perhaps you&apos;d like to <Link href="/draw"> draw</Link>?
