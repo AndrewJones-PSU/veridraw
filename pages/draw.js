5a5e1c2199e31edbb9c7c9e3332f1d06af455a5d
AndrewJones-PSU <aej5218@psu.edu>
2022-03-07 23:29:17 -0500

    I think I got it now

--- pages/draw.js
+++ pages/draw.js
@@ -1,6 +1,10 @@
 import styles from 'draw.module.css'
 import Link from 'next/link'
 
-<p>
-    There&apos;s nothing here right now, perhaps you&apos;d like to <Link href="/draw"> draw</Link>?
-</p>
+export function draw() {
+    return (
+        <p>
+            There&apos;s nothing here right now, perhaps you&apos;d like to <Link href="/draw"> draw</Link>?
+        </p>
+    )
+}
